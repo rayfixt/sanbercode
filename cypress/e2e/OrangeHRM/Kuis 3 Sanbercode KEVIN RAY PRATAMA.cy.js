14183cf053968b7fabf0911ae3ee0da783ad6a12
Lintang Dila Mutiara <kevinraypratama@gmail.com>
2025-08-25 16:40:06 +0700

    push trial

---
+++ cypress/e2e/OrangeHRM/Kuis 3 Sanbercode KEVIN RAY PRATAMA.cy.js
@@ -0,0 +1,71 @@
+describe ('Scenario Login', () => {
+    it ('TC001-Login dengan Credential Valid',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC002-Login dengan Credential Invalid Username',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('nimda')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC003-Login dengan Credential Invalid Password',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin1234')
+        cy.get('.oxd-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC004-Forgot Password',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('.orangehrm-login-forgot > .oxd-text').click()
+        cy.get('[name="username"]').type('Admin')
+        cy.get('.oxd-button--secondary').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC005-Help/ FAQ',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-topbar-body-nav-slot > .oxd-icon-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC006-Support',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-userdropdown-tab').click()
+        cy.get(':nth-child(2) > .oxd-userdropdown-link').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC007-Ubah Password',() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-userdropdown-tab').click()
+        cy.get(':nth-child(3) > .oxd-userdropdown-link').click()
+        cy.get(':nth-child(1) > .oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin12345')
+        cy.get('.user-password-row > .oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin12345')
+        cy.get('.oxd-button--secondary').click()
+    })
+})
---
+++ cypress/e2e/Saucedemo SWAGLABS/Login.cy.js
@@ -0,0 +1,42 @@
+describe ('Scenario Login', () => {
+    it ('TC001-Login dengan credential valid',() => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC002-Login dengan credential invalid username',() => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('[data-test="username"]').type('standar_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC003-Login dengan credential invalid password',() => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauced')
+        cy.get('#login-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC004-Login dengan mengosongkan username',() => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('#login-button').click()
+    })
+})
+
+describe ('Scenario Login', () => {
+    it ('TC004-Login dengan mengosongkan password',() => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('#login-button').click()
+    })
+})
